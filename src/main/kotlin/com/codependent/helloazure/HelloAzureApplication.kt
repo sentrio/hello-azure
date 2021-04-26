efcf7461eb22471e1e15e9574a7613694b4b4ef7
jainigo <jainigo@profile.es>
2021-04-26 15:05:31 +0200

    PI-1017 sample project

---
+++ src/main/kotlin/com/codependent/helloazure/HelloAzureApplication.kt
@@ -0,0 +1,19 @@
+package com.codependent.helloazure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class HelloAzureApplication{
+
+    @RequestMapping
+    fun hello() = "Hello Azure!"
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<HelloAzureApplication>(*args)
+}
---
+++ src/test/kotlin/com/codependent/helloazure/HelloAzureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.codependent.helloazure
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloAzureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
